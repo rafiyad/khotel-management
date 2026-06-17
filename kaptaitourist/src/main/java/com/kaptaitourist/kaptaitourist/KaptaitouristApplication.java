37bb7281d23a4e4b897340f199fe755ae5ce64f6
Rafiyad <rafiyad.celloscope@gmail.com>
2026-06-17 18:35:29 +0600

    first commit

---
+++ kaptaitourist/src/main/java/com/kaptaitourist/kaptaitourist/KaptaitouristApplication.java
@@ -0,0 +1,13 @@
+package com.kaptaitourist.kaptaitourist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaptaitouristApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaptaitouristApplication.class, args);
+	}
+
+}
---
+++ kaptaitourist/src/test/java/com/kaptaitourist/kaptaitourist/KaptaitouristApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaptaitourist.kaptaitourist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaptaitouristApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
